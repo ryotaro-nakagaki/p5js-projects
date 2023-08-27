11ff977cce8f461ad0caa8eceb40b1a711a2f450
ryotaro-nakagaki <gakidtm@gmail.com>
2023-08-27 22:38:59 +0900

    23082502_AudioVisualizerの修正を反映

--- 23082502_AudioVisualizer/sketch.js
+++ 23082502_AudioVisualizer/sketch.js
@@ -1,36 +1,63 @@
+const w = 1440 / 8, h = 1080 / 8
+const fr = 6
+
 function preload() {
   sound = loadSound('./audio/audio.wav')
 }
 
 function setup() {
-  createCanvas(1080 / 4, 1080 / 4)
-
-  frameRate(60)
-
-  // fft = new p5.FFT()
+  createCanvas(w, h)
+  frameRate(fr)
+  fft = new p5.FFT()
 
   noLoop()
 }
 
 function draw() {
-  // let waveform = fft.waveform()
+  FRAME_WIDTH = min(width, height) / 10
 
-  // beginShape()
-  // for (let i = 0; i < waveform.length; i++) {
-  //   let x = map(i, 0, waveform.length, 0, width)
-  //   let y = map(waveform[i], -1, 1, 0, height)
-  //   vertex(x, y)
-  // }
-  // endShape()
+  {
+    stroke("black")
+    strokeWeight(1)
+    noFill()
+  }
 
-  background("white")
-  text(frameCount, width / 2, height / 2)
+  {
+    background("white")
+
+    drawWaveform(FRAME_WIDTH, FRAME_WIDTH, width - FRAME_WIDTH, height - FRAME_WIDTH)
+
+    // text(frameCount, FRAME_WIDTH, FRAME_WIDTH)
+
+    addFrame("white", "black", FRAME_WIDTH)
+    addPaperTexture(true, false, true, false)
+  }
+
+  saveCanvas(("0000000" + frameCount).slice(-7), "png")
+
+  if (frameCount === ceil(getTargetFrameRate() * sound.duration())) {
+    noLoop()
+    sound.stop()
+  }
+
 }
 
-function keyPressed() {
+function keyTyped() {
   if (key === 's') {
-    saveGif('mySketch', getTargetFrameRate() * sound.duration(), { units: "frames" })
     loop()
     sound.play()
   }
+}
+
+function drawWaveform(x1, y1, x2, y2) {
+  const waveform = fft.waveform(256)
+  console.log(waveform.length)
+
+  beginShape()
+  for (let i = 0; i < 256; i++) {
+    let x = map(i, 0, 256, x1, x2)
+    let y = map(waveform[i], -1, 1, y2, y1)
+    curveVertex(x, y)
+  }
+  endShape()
 }
